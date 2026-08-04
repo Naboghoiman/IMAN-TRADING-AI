fa4dd653cce7403793bf10bbeaf13dd6e1b1bd7c
Naboghoiman <156505016+Naboghoiman@users.noreply.github.com>
2026-08-04 07:24:28 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,37 @@
-from main import *
+from market_data import get_eth_candles
+from indicators import add_indicators
+from signal_engine import generate_signal
+import time
 
-print("🤖 IMAN TRADING AI APPLICATION READY")
+
+print("🤖 IMAN TRADING AI APPLICATION STARTED")
+
+
+while True:
+
+    try:
+        df = get_eth_candles()
+
+        df = add_indicators(df)
+
+        result = generate_signal(df)
+
+        price = df.iloc[-1]["close"]
+
+        print("========================")
+        print("ETH/USD PRICE:", price)
+        print("SIGNAL:", result["SIGNAL"])
+        print("CONFIDENCE:", result["CONFIDENCE"], "%")
+        print("REASONS:")
+
+        for r in result["REASONS"]:
+            print("-", r)
+
+        print("========================")
+        print("Next scan in 5 minutes...")
+
+        time.sleep(300)
+
+    except Exception as e:
+        print("ERROR:", e)
+        time.sleep(60)
