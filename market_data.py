099265dc3e0bd374b802a713e16b4ab9d838fa68
Naboghoiman <156505016+Naboghoiman@users.noreply.github.com>
2026-08-04 09:33:15 +0300

    Update market_data.py

--- market_data.py
+++ market_data.py
@@ -1,23 +1,44 @@
 import requests
 import pandas as pd
 
+
 def get_eth_candles():
+
     url = "https://api.binance.com/api/v3/klines"
-    
+
     params = {
         "symbol": "ETHUSDT",
         "interval": "5m",
         "limit": 100
     }
 
-    data = requests.get(url, params=params).json()
+    response = requests.get(url, params=params, timeout=10)
 
-    df = pd.DataFrame(data, columns=[
-        "time","open","high","low","close",
-        "volume","close_time",
-        "qav","trades",
-        "tb_base","tb_quote","ignore"
-    ])
+    print("BINANCE STATUS:", response.status_code, flush=True)
+
+    data = response.json()
+
+    if not isinstance(data, list):
+        print("BINANCE ERROR:", data, flush=True)
+        return pd.DataFrame()
+
+    df = pd.DataFrame(
+        data,
+        columns=[
+            "time",
+            "open",
+            "high",
+            "low",
+            "close",
+            "volume",
+            "close_time",
+            "qav",
+            "trades",
+            "tb_base",
+            "tb_quote",
+            "ignore"
+        ]
+    )
 
     df["close"] = df["close"].astype(float)
     df["open"] = df["open"].astype(float)
