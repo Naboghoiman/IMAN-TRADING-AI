02702f961739686e0e54e53a321b1466c02fa8d9
Naboghoiman <156505016+Naboghoiman@users.noreply.github.com>
2026-08-04 08:26:52 +0300

    Update run.py
    Fix Render web server

--- run.py
+++ run.py
@@ -1,14 +1,33 @@
+from flask import Flask
+import threading
+import time
+
 from market_data import get_eth_candles
 from indicators import add_indicators
 from signal_engine import generate_signal
-import time
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "IMAN TRADING AI ONLINE"
+
+
+def start_web():
+    app.run(host="0.0.0.0", port=10000)
 
 
 print("🤖 IMAN TRADING AI APPLICATION STARTED")
-
 print("✅ IMAN TRADING AI ONLINE")
-while True:
 
+
+# Start Render web server
+threading.Thread(target=start_web).start()
+
+
+while True:
     try:
         df = get_eth_candles()
 
@@ -18,16 +37,16 @@ while True:
 
         price = df.iloc[-1]["close"]
 
-        print("========================")
+        print("==========================")
         print("ETH/USD PRICE:", price)
         print("SIGNAL:", result["SIGNAL"])
-        print("CONFIDENCE:", result["CONFIDENCE"], "%")
+        print("CONFIDENCE:", result["CONFIDENCE"])
         print("REASONS:")
 
         for r in result["REASONS"]:
             print("-", r)
 
-        print("========================")
+        print("==========================")
         print("Next scan in 5 minutes...")
 
         time.sleep(300)
