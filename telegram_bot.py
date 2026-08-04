845d60fd409245c72ff7e89dbcbf1d6427e507bf
Naboghoiman <156505016+Naboghoiman@users.noreply.github.com>
2026-08-04 12:59:49 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,56 +1,53 @@
-# IMAN TRADING AI TELEGRAM BOT
 
 import os
 import requests
-import telebot
+
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
-
-bot = telebot.TeleBot(TOKEN)
-
-CHAT_ID_FILE = "chat_id.txt"
-
-
-@bot.message_handler(commands=["start"])
-def start(message):
-    chat_id = message.chat.id
-
-    with open(CHAT_ID_FILE, "w") as f:
-        f.write(str(chat_id))
-
-    bot.send_message(
-        chat_id,
-        "✅ IMAN TRADING AI connected successfully.\n\n"
-        f"Your Chat ID is: {chat_id}"
-    )
-
-    print("CHAT ID SAVED:", chat_id)
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 
 def send_alert(message):
+
+    print("TOKEN EXISTS:", bool(TOKEN), flush=True)
+    print("CHAT ID:", CHAT_ID, flush=True)
+
+    if not TOKEN:
+        print("ERROR: TELEGRAM_TOKEN missing", flush=True)
+        return
+
+    if not CHAT_ID:
+        print("ERROR: TELEGRAM_CHAT_ID missing", flush=True)
+        return
+
+
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    data = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+
+
     try:
-        if not os.path.exists(CHAT_ID_FILE):
-            print("NO CHAT ID FOUND")
-            return
 
-        with open(CHAT_ID_FILE, "r") as f:
-            chat_id = f.read().strip()
+        response = requests.post(
+            url,
+            data=data,
+            timeout=10
+        )
 
-        url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+        print(
+            "TELEGRAM RESPONSE:",
+            response.text,
+            flush=True
+        )
 
-        data = {
-            "chat_id": chat_id,
-            "text": message
-        }
-
-        response = requests.post(url, data=data)
-
-        print("TELEGRAM RESPONSE:", response.text)
 
     except Exception as e:
-        print("TELEGRAM ERROR:", e)
 
-
-print("TELEGRAM BOT RUNNING")
-
-bot.infinity_polling()
+        print(
+            "TELEGRAM ERROR:",
+            e,
+            flush=True
+        )
