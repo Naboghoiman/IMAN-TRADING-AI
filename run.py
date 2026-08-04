8f3a2ba7699be7651351fcc68a18937816c25e4d
Naboghoiman <156505016+Naboghoiman@users.noreply.github.com>
2026-08-04 11:23:13 +0300

    Update run.py

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import time
 from market_data import get_eth_candles
 from indicators import add_indicators
 from signal_engine import generate_signal
+from telegram_bot import send_alert
 
 
 app = Flask(__name__)
@@ -19,7 +20,7 @@ def start_web():
     app.run(host="0.0.0.0", port=10000)
 
 
-print("🤖 IMAN TRADING AI APPLICATION STARTED 🚀", flush=True)
+print("🤖 IMAN TRADING AI APPLICATION STARTED")
 
 
 # Start Render web server
@@ -27,20 +28,26 @@ threading.Thread(target=start_web).start()
 
 
 while True:
+
     print("AI LOOP STARTED", flush=True)
 
     try:
+
         print("FETCHING CANDLES...", flush=True)
 
         df = get_eth_candles()
 
         print("DATA SIZE:", len(df), flush=True)
 
+
         df = add_indicators(df)
+
 
         result = generate_signal(df)
 
+
         price = df.iloc[-1]["close"]
+
 
         print("==============================")
         print("ETH/USD PRICE:", price)
@@ -48,14 +55,41 @@ while True:
         print("CONFIDENCE:", result["CONFIDENCE"])
         print("REASONS:")
 
+
         for r in result["REASONS"]:
             print("-", r)
 
+
+        message = f"""
+🤖 IMAN TRADING AI
+
+📊 ETH/USD
+
+SIGNAL: {result["SIGNAL"]}
+CONFIDENCE: {result["CONFIDENCE"]}%
+
+PRICE: {price}
+
+REASONS:
+{chr(10).join(result["REASONS"])}
+
+Next scan in 5 minutes...
+"""
+
+
+        send_alert(message)
+
+
         print("==============================")
+        print("Telegram alert sent", flush=True)
         print("Next scan in 5 minutes...", flush=True)
+
 
         time.sleep(300)
 
+
     except Exception as e:
+
         print("ERROR:", e, flush=True)
+
         time.sleep(60)
